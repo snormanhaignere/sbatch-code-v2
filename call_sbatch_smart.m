df2ac027dc5cd667760f3470046f18349d3322f0
Sam Norman-Haignere <snormanhaignere@gmail.com>
2017-03-17 15:48:00 -0400

    Minor change: call_sbatch_smart.m
    The function now returns with a job was actually run.

--- call_sbatch_smart.m
+++ call_sbatch_smart.m
@@ -1,7 +1,10 @@
-function call_sbatch_smart(B)
+function job_started = call_sbatch_smart(B)
+
+% 2017-03-17: Modified to return whether job successfully started
 
 % check if the job is already running
 if is_job_running(B.job_id);
+    job_started = false;
     return;
 end
 
@@ -11,3 +14,4 @@ wait_until_fewer_than_N_processes_are_running('svnh', B.max_num_process);
 
 % call sbatch
 call_sbatch(B);
+job_started = true;
